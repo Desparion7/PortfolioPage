f84f7aeb9b295547a1a157fb1cf9447509751f99
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-01-16 15:11:32 +0100

    add typing welcome page

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,10 +1,83 @@
-import React from 'react'
-import './Home.css'
+import React, { useEffect } from 'react';
+import Typewriter from 'typewriter-effect';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div className='home'></div>
-  )
-}
+	useEffect(() => {
+		const cursors = document.querySelectorAll('.Typewriter__cursor');
+		cursors.forEach((cursor) => {
+			cursor.style.display = 'none';
+		});
+	});
 
-export default Home
+	return (
+		<div className='home'>
+			<div className='home__container'>
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[0].style.display = 'inline-block';
+							})
+							.changeDelay(100)
+							.typeString('Welcome on my page.')
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[0].style.display = 'none';
+							})
+							.start();
+					}}
+				/>
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.pauseFor(2400)
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[1].style.display = 'inline-block';
+							})
+							.changeDelay(100)
+							.typeString('My name is Mateusz Woś.')
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[1].style.display = 'none';
+							})
+							.start();
+					}}
+				/>
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.pauseFor(5700)
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[2].style.display = 'inline-block';
+							})
+							.changeDelay(100)
+							.typeString(`I'm a beginner front-end web developer.`)
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[2].style.display = 'none';
+							})
+							.start();
+					}}
+				/>
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.pauseFor(10000)
+							.callFunction(() => {
+								const cursor = document.querySelectorAll('.Typewriter__cursor');
+								cursor[3].style.display = 'inline-block';
+							})
+							.typeString(`Check out my page to  know me better.`)
+							.start();
+					}}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
